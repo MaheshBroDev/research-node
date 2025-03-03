14e6324a593c2e213cae9f5f95ead03df80ca881
MaheshBroDev <maheshkcot95@gmail.com>
2025-03-03 22:35:10 +0530

    fixes

--- app.js
+++ app.js
@@ -338,14 +338,8 @@ app.get('/performance/last', performanceLoggingMiddleware('/performance/last'), 
 // Endpoint to serve text files from /loader/<path>.txt
 app.get('/loaderio-:filename([a-zA-Z0-9]{32}).txt', (req, res) => {
     const filename = req.params.filename;
-    const filePath = `/loaderio-${filename}.txt`;
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(404).send('File not found');
-        }
-        res.set('Content-Type', 'text/plain');
-        res.send(data);
-    });
+    res.set('Content-Type', 'text/plain');
+    res.send(`loaderio-${filename}`);
 });
 
 // Graceful Shutdown
